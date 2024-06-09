c5873a3947363712497ffc8adf3e7aec42d2d6a0
Kiengabby <kiengabby@gmail.com>
2024-06-10 03:03:18 +0700

    user

--- src/app/pages/user/quan-ly-thiet-bi/quan-ly-thiet-bi.component.ts
+++ src/app/pages/user/quan-ly-thiet-bi/quan-ly-thiet-bi.component.ts
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./quan-ly-thiet-bi.component.less']
 })
 export class QuanLyThietBiComponent {
+  value : string
+  listOfData: any[] = [
+    {
+      tenThietBi: '1',
+      maThietBi: 'John Brown',
+      soLuong: 32,
+      ngayNhapVe: new Date(),
+      ngayBaoHanh: new Date(),
+      xuatXu: 'abc',
+      tinhTrang: 'moi'
+    },
+  ];
+  tenThietBi: any;
+  maThietBi: string;
+  soLuong: number;
+  ngayNhapVe: Date;
+  ngayBaoHanh: Date;
+  xuatXu: string;
+  tinhTrang: string;
 
+  themMoi() {
+    // 1 object cos thong tin -> add mang
+    // ham them manbg push
+    const input = {
+      tenThietBi: this.tenThietBi,
+      maThietBi: this.maThietBi,
+      soLuong: this.soLuong,
+      ngayNhapVe: this.ngayNhapVe,
+      ngayBaoHanh: this.ngayBaoHanh,
+      xuatXu: this.xuatXu,
+      tinhTrang: this.tinhTrang,
+    }
+    this.listOfData.push(input);
+    console.log(this.listOfData);
+  }
 }
